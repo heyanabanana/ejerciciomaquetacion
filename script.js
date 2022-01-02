5d45f430a51c7753c9138f92045022f8d67f87ae
Ana Fernández Valdés <aana.fdez@gmail.com>
2022-01-02 10:43:47 +0100

    styles

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  dots:true,
-  nav:true,
-  // autoplay:true,   
-  // smartSpeed: 3000, 
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+
+  // autoplay:true,
+  // smartSpeed: 3000,
   // autoplayTimeout:7000,
-items: 1
-})
+  items: 1,
+});
+$(".owl-next").html(
+  '<img class="arrow-icon arrow-icon-next" src="assets/icons/nav-icon-right.svg"/>'
+);
+$(".owl-prev").html(
+  '<img class="arrow-icon arrow-icon-next" src="assets/icons/nav-icon-left.svg"/>'
+);
