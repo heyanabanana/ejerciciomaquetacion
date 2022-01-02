b8762b74e357f2b46026fa3c3de59d8f52a905f5
Ana Fernández Valdés <91973567+heyanabanana@users.noreply.github.com>
2022-01-02 18:47:07 +0100

    add responsive

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-$(".owl-carousel").owlCarousel({
+$(document).ready(function() {
+  var owl = $('.owl-carousel');
+
+owl.owlCarousel({
   loop: true,
   margin: 10,
   nav: true,
-
-  // autoplay:true,
+  autoplay:true,
   // smartSpeed: 3000,
-  // autoplayTimeout:7000,
+  autoplayTimeout:5000,
   items: 1,
+  animateOut: 'fadeOut'
+
 });
 $(".owl-next").html(
   '<img class="arrow-icon arrow-icon-next" src="assets/icons/nav-icon-right.svg"/>'
@@ -14,3 +18,14 @@ $(".owl-next").html(
 $(".owl-prev").html(
   '<img class="arrow-icon arrow-icon-next" src="assets/icons/nav-icon-left.svg"/>'
 );
+
+owl.on('translate.owl.carousel', function(e) {
+  var index = e.item.index;
+  $('.card_image').removeClass('animate__animated animate__fadeIn animate__delay-2s');
+  $('.card_image').eq(index).addClass('animate__animated animate__fadeIn animate__delay-2s');
+  $('.card_details').removeClass('animate__animated animate__fadeIn animate__delay-1s');
+  $('.card_details').eq(index).addClass('animate__animated animate__fadeIn animate__delay-1s');
+
+});
+
+});
